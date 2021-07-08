71667fc45c40a08b2db18ed89db4eec8d7f0e923
carloscavalcante <carlos.xande115@gmail.com>
2021-07-08 19:06:30 -0300

    Update ouvir.py

--- ouvir.py
+++ ouvir.py
@@ -1,14 +1,24 @@
+#!/usr/bin/python
+import io
+import os
+import json
+from asterisk.agi import *
 import speech_recognition as sr
+import sys
+
+agi = AGI()
+
+idligacao = agi.get_variable('id')
 #Funcao responsavel por ouvir e reconhecer a fala
 def ouvir_microfone():
      #Habilita o microfone para ouvir o usuario
      r = sr.Recognizer()
-     microfone = sr.AudioFile('/var/lib/asterisk/sounds/.wav')
+     microfone = sr.AudioFile('/var/spool/asterisk/monitor/speech/'+str(idligacao)+'.wav')
      #Passa o audio para o reconhecedor de padroes do speech_recognition
      with microfone as source:
       audio = r.record(source)
       frase = r.recognize_google(audio,language='pt-BR')
-      #Após alguns segundos, retorna a frase falada
-      print("Você disse: " + frase)
      return frase
-ouvir_microfone()
+fala=ouvir_microfone()
+agi.set_variable("OUTPUT",fala)
+os.remove('/var/spool/asterisk/monitor/speech/'+str(idligacao)+'.wav')
